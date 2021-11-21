d354e4b12219173d98ba902e32d7d4fb2e4d302a
Siya117 <78736374+Siya117@users.noreply.github.com>
2021-11-21 13:04:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+/*created by prashant shukla */
 
 var paddle2 =10,paddle1=10;
 
@@ -25,9 +25,14 @@ rightWristY = 0;
 rightWristX = 0;
 scoreRightWrist = 0;
 
-status = "";//Define a variable to hold the status of the game
+game_status = "";
 
- 
+//var doggo_theme = new Audio("https://saantonandre.github.io/doggo_theme.wav");
+
+ function preload() {
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
 
 function setup(){
 var canvas =  createCanvas(700,600);
@@ -59,13 +64,12 @@ function gotPoses(results)
 
 function startGame()
 {
-  game_status = "start";//Set the value of the status variable created in step 1 to “start”.
-  document.getElementById("status").innerHTML = "Game Is Loading";//Update the h3 tag which we have created inside index.html file in project 138 who has id “status” to "Game Is Loaded”.
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loading";
 }
 
 function draw(){
-if(game_status == "start") // inside the if condition check if the game_status is equal to the value "start".
-{
+
   background(0); 
   image(video, 0, 0, 700, 600);
 
@@ -84,7 +88,9 @@ if(game_status == "start") // inside the if condition check if the game_status i
     circle(rightWristX, rightWristY, 30);
   }
 
-
+  if(game_status == "start")
+  {
+    document.getElementById("status").innerHTML = "Game Is Loaded";
     //funtion paddleInCanvas call 
     paddleInCanvas();
         
@@ -166,11 +172,11 @@ function move(){
   if (ball.x-2.5*ball.r/2< 0){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
     ball.dx = -ball.dx+0.5; 
-    
+    ball_touch_paddel.play();
   }
   else{
     pcscore++;
-    
+    missed.play();
     reset();
     navigator.vibrate(100);
   }
@@ -183,10 +189,10 @@ if(pcscore ==4){
     stroke("white");
     textSize(25);
     text("Game Over!",width/2,height/2);
-    text("Reload the page!",width/2,height/2+30)
+    text("Press Restart button to play again!",width/2,height/2+30)
     noLoop();
     pcscore = 0;
- }
+}
    if(ball.y+ball.r > height || ball.y-ball.r <0){
        ball.dy =- ball.dy;
    }   
@@ -206,13 +212,18 @@ function models(){
 
 //this function help to not go te paddle out of canvas
 function paddleInCanvas(){
-  if(paddle1Y+paddle1Height > height){
-    paddle1Y=height-paddle1Height;
+  if(mouseY+paddle1Height > height){
+    mouseY=height-paddle1Height;
   }
-  if(paddle1Y < 0){
-    paddle1Y =0;
+  if(mouseY < 0){
+    mouseY =0;
   }
  
   
 }
 
+function restart()
+{
+  pcscore = 0;
+  loop();
+}
